4b18d1c1e94137cbaa0c0bf10a38659aadba1235
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 15:52:34 +0000

    sss

--- pages/upload_pdf.py
+++ pages/upload_pdf.py
@@ -5,6 +5,8 @@ import streamlit_sparrow_labeling
 from streamlit_sparrow_labeling import st_sparrow_labeling
 # Set page width to half of the screen width
 PAGE_WIDTH =  st_js.st_javascript("window.innerWidth")/2
+PAGE_HEIGHT =  st_js.st_javascript("window.innerHeight")/2
+
 
 st.title("PDF Viewer")
 
@@ -33,8 +35,10 @@ if pdf_file is not None:
         pix.save(val)
         height = 1296
         width = 864
+        doc_height = saved_state['meta']['image_size']['height']
+        doc_width = saved_state['meta']['image_size']['width']
         canvas_width = canvas_available_width(ui_width)
-        result_rects = st_sparrow_labeling(fill_color="rgba(0, 151, 255, 0.3)",stroke_width=2, stroke_color="rgba(0, 50, 255, 0.7)",background_image=val, drawing_mode=mode, display_toolbar=True, update_streamlit=True, image_rescale=True, key="doc_annotation" )
+        result_rects = st_sparrow_labeling(fill_color="rgba(0, 151, 255, 0.3)",stroke_width=2, stroke_color="rgba(0, 50, 255, 0.7)",background_image=val, drawing_mode=mode, display_toolbar=True, update_streamlit=True, canvas_width=PAGE_WIDTH, doc_height=PAGE_HEIGHT, doc_width=PAGE_WIDTH, image_rescale=True, key="doc_annotation" )
 
         # st.image(val)
     with col2:
