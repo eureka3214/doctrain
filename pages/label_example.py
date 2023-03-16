8102f55454d8e1fd144214b564d892f6a246c620
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 17:16:19 +0000

    nnn

--- pages/label_example.py
+++ pages/label_example.py
@@ -13,10 +13,16 @@ from streamlit_sparrow_labeling import DataProcessor
 import json
 import math
 
-st.set_page_config(page_title="Sparrow Labeling", layout="wide")
+
 def run(img_file, rects_file, labels):
-    ui_width = st_js.st_javascript("window.innerWidth")
+    
     docImg = Image.open(img_file)
+    if 'saved_state' not in st.session_state:
+        with open(rects_file, "r") as f:
+            saved_state = json.load(f)
+            st.session_state['saved_state'] = saved_state
+    else:
+        saved_state = st.session_state['saved_state']
     assign_labels = st.checkbox("Assign Labels", True)
     mode = "transform" if assign_labels else "rect"
     data_processor = DataProcessor()
@@ -28,7 +34,7 @@ def run(img_file, rects_file, labels):
         doc_height = 708
         doc_width = 510
         canvas_width = canvas_available_width(ui_width)
-        result_rects = st_sparrow_labeling(fill_color="rgba(0, 151, 255, 0.3)",stroke_width=2, stroke_color="rgba(0, 50, 255, 0.7)",background_image=docImg, height=height,width=width, drawing_mode=mode, display_toolbar=True, update_streamlit=True, canvas_width=canvas_width, doc_height=doc_height, doc_width=doc_width, image_rescale=True, key="doc_annotation" )
+        result_rects = st_sparrow_labeling(fill_color="rgba(0, 151, 255, 0.3)",stroke_width=2, stroke_color="rgba(0, 50, 255, 0.7)",background_image=docImg, initial_rects=saved_state, height=height,width=width, drawing_mode=mode, display_toolbar=True, update_streamlit=True, canvas_width=canvas_width, doc_height=doc_height, doc_width=doc_width, image_rescale=True, key="doc_annotation" )
         st.caption("Check 'Assign Labels' to enable editing of labels and values, move and resize the boxes to annotate the document.")
         st.caption("Add annotations by clicking and dragging on the document, when 'Assign Labels' is unchecked.")
 
@@ -53,7 +59,7 @@ def run(img_file, rects_file, labels):
                         json.dump(result_rects.rects_data, f, indent=2)
                     with open(rects_file, "r") as f:
                         saved_state = json.load(f)
-                       
+                        st.session_state['saved_state'] = saved_state
                     st.write("Saved!")
 
 
@@ -134,9 +140,9 @@ def canvas_available_width(ui_width):
 if __name__ == "__main__":
     # custom_labels = ["", "paragraph", "Topic", "Subtopic", "Objective", "SubtopicContents"]
     # run("docs/image/download.png", "docs/json/download.json", custom_labels)
-    # ui_width = st_js.st_javascript("window.innerWidth")
+    ui_width = st_js.st_javascript("window.innerWidth")
     # Set page width to half of the screen width
-    # PAGE_WIDTH = ui_width/2
+    PAGE_WIDTH = ui_width/2
 
     st.title("PDF Viewer")
 
