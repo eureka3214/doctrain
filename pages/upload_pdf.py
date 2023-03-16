905cac59a67ab4af9a5b6d94acbebfa08de76613
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 14:24:49 +0000

    xxx

---
+++ pages/upload_pdf.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import fitz  # PyMuPDF
+
+# Set page width to half of the screen width
+PAGE_WIDTH = int(st.beta_get_window_width() / 2)
+
+def main():
+    st.title("PDF Viewer")
+
+    # File uploader
+    uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
+
+    # Page number input
+    page_num = st.number_input("Enter a page number", value=1, min_value=1, max_value=get_num_pages(uploaded_file), step=1)
+
+    # Display selected page
+    if uploaded_file is not None:
+        # Load PDF document
+        doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+
+        # Get selected page
+        page = doc.load_page(page_num - 1)
+
+        # Render page as image and display on left half of screen
+        with st.container():
+            st.image(page.get_pixmap(alpha=False), width=PAGE_WIDTH)
+
+        # Clean up
+        doc.close()
+
+def get_num_pages(uploaded_file):
+    """
+    Helper function to get the number of pages in a PDF document.
+    """
+    if uploaded_file is not None:
+        # Load PDF document
+        doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+        num_pages = doc.page_count
+
+        # Clean up
+        doc.close()
+
+        return num_pages
+    else:
+        return 0
+
+if __name__ == "__main__":
+    main()
