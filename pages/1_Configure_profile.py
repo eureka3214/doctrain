32f10a18a85d400f16175f990b81074ae6904abd
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 23:50:11 +0000

    dddd

--- pages/1_Configure_profile.py
+++ pages/1_Configure_profile.py
@@ -5,6 +5,9 @@ default_data = {
     "meta": {
         "version": "v0.1",
         "name": "Indian Bank Structure",
+        "labels": [
+            ""
+        ],
         "image_id": 1001,
         "image_size": {
             "width": 510,
@@ -30,10 +33,19 @@ def app():
     version = default_data['meta']['version']
     new_version = f"v{float(version[1:])+0.1:.1f}"
     split = st.text_input("Meta Config Profile Name", default_data["meta"]["name"])
+    
+    # Add labels dynamically
+    labels = []
+    while st.button("Add Label"):
+        label = st.text_input("Label Name")
+        if label:
+            labels.append(label)
+    
     new_data = {
         "meta": {
             "version": new_version,
-            "split": split,
+            "name": split,
+            "labels": labels,
             "image_id": "",
             "image_size": {
                 "width": 510,
@@ -53,6 +65,7 @@ def app():
             }
         ]
     }
+    
     st.code(json.dumps(new_data, indent=4))
 
 app()
