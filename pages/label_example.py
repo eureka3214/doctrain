c80fa3835f69164390ee7c9e0e74c0395c2f296d
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 16:05:51 +0000

    sss

--- pages/label_example.py
+++ pages/label_example.py
@@ -1,47 +1,47 @@
-from PIL import Image
-import streamlit as st
-from streamlit_sparrow_labeling import st_sparrow_labeling
-import json
-import streamlit_javascript as st_js
+# from PIL import Image
+# import streamlit as st
+# from streamlit_sparrow_labeling import st_sparrow_labeling
+# import json
+# import streamlit_javascript as st_js
 
 
-st.set_page_config(page_title="Sparrow Labeling", layout="wide")
+# st.set_page_config(page_title="Sparrow Labeling", layout="wide")
 
-def run(img_file, label_data_file):
-    ui_width = st_js.st_javascript("window.innerWidth")
-    img = Image.open(img_file)
+# def run(img_file, label_data_file):
+#     ui_width = st_js.st_javascript("window.innerWidth")
+#     img = Image.open(img_file)
     
-    if 'saved_state' not in st.session_state:
-        with open(label_data_file, "r") as f:
-            saved_state = json.load(f)
-            st.session_state['saved_state'] = saved_state
-    else:
-        saved_state = st.session_state['saved_state']
+#     if 'saved_state' not in st.session_state:
+#         with open(label_data_file, "r") as f:
+#             saved_state = json.load(f)
+#             st.session_state['saved_state'] = saved_state
+#     else:
+#         saved_state = st.session_state['saved_state']
     
-    result_rects = st_sparrow_labeling(
-        fill_color="rgba(0, 151, 255, 0.3)",
-        stroke_width=2,
-        stroke_color="rgba(0, 50, 255, 0.7)",
-        background_image=img,
-        initial_rects=saved_state,
-        drawing_mode="rect",
-        display_toolbar=True,
-        update_streamlit=True,
-        canvas_width=ui_width,
-        key="doc_annotation"
-    )
+#     result_rects = st_sparrow_labeling(
+#         fill_color="rgba(0, 151, 255, 0.3)",
+#         stroke_width=2,
+#         stroke_color="rgba(0, 50, 255, 0.7)",
+#         background_image=img,
+#         initial_rects=saved_state,
+#         drawing_mode="rect",
+#         display_toolbar=True,
+#         update_streamlit=True,
+#         canvas_width=ui_width,
+#         key="doc_annotation"
+#     )
     
-    if result_rects is not None:
-        with open(label_data_file, "w") as f:
-            json.dump(result_rects.rects_data, f, indent=2)
-        with open(label_data_file, "r") as f:
-            saved_state = json.load(f)
-            st.session_state['saved_state'] = saved_state
-        st.write("Saved!")
+#     if result_rects is not None:
+#         with open(label_data_file, "w") as f:
+#             json.dump(result_rects.rects_data, f, indent=2)
+#         with open(label_data_file, "r") as f:
+#             saved_state = json.load(f)
+#             st.session_state['saved_state'] = saved_state
+#         st.write("Saved!")
     
-    st.image(img)
+#     st.image(img)
 
-img_file = "docs/image/download.png"
-label_data_file = "docs/json/download.json"
-run(img_file, label_data_file)
+# img_file = "docs/image/download.png"
+# label_data_file = "docs/json/download.json"
+# run(img_file, label_data_file)
 
