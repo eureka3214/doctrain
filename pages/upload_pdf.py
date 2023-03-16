670182780bdb5a2aa920e535a6d9188edf611439
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 15:53:47 +0000

    sss

--- pages/upload_pdf.py
+++ pages/upload_pdf.py
@@ -37,7 +37,7 @@ if pdf_file is not None:
         width = 864
         # doc_height = saved_state['meta']['image_size']['height']
         # doc_width = saved_state['meta']['image_size']['width']
-        canvas_width = canvas_available_width(ui_width)
+        # canvas_width = canvas_available_width(ui_width)
         result_rects = st_sparrow_labeling(fill_color="rgba(0, 151, 255, 0.3)",stroke_width=2, stroke_color="rgba(0, 50, 255, 0.7)",background_image=val, drawing_mode=mode, display_toolbar=True, update_streamlit=True, canvas_width=PAGE_WIDTH, doc_height=PAGE_HEIGHT, doc_width=PAGE_WIDTH, image_rescale=True, key="doc_annotation" )
 
         # st.image(val)
