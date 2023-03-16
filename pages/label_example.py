9803473f70ee206d95ba508bc18bfaa162a499e1
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-16 16:43:52 +0000

    jjj

--- pages/label_example.py
+++ pages/label_example.py
@@ -167,10 +167,10 @@ if __name__ == "__main__":
             val = f"image_{page_number}.png"
             page = doc.load_page(page_number-1)  # Page numbers start from 0 in PyMuPDF
             pix = page.get_pixmap(matrix=mat)
-            pat = pix.save(val)
+            pix.save(val)
             # st.image(val)
             custom_labels = ["", "paragraph", "Topic", "Subtopic", "Objective", "SubtopicContents"]
-            run( pat, "docs/json/download.json", custom_labels)
+            run( val, "docs/json/download.json", custom_labels)
         with col2:
             page_number = st.number_input("Page number", min_value=1, max_value=count, value=page_number, step=1)    
         # Clean up
