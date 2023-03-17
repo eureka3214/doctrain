2721fc5e79345eaeb29a77d519a1913b429c465c
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-17 04:01:42 +0000

    mmmm

---
+++ pages/3_Download_Data.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import json
+import base64
+session_state = st.session_state
+json_files = session_state.jsonlist
+# Define a list of JSON file paths
+# json_files = ["file1.json", "file2.json", "file3.json"]
+
+# Define a function to download multiple JSON files
+def download_multiple_files(json_files):
+    compressed_file = BytesIO()
+    with zipfile.ZipFile(compressed_file, "w") as archive:
+        for json_file in json_files:
+            data = Path(json_file).read_text()
+            archive.writestr(json_file, data)
+    compressed_file.seek(0)
+    b64 = base64.b64encode(compressed_file.getvalue()).decode()
+    href = f'<a href="data:application/zip;base64,{b64}" download="multiple_files.zip">Download JSON Files</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+# Display the download button
+download_multiple_files(json_files)
