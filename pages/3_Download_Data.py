b72b002a07adf2fe6edaabd724fb411a928d4dfb
eureka3214 <127386346+eureka3214@users.noreply.github.com>
2023-03-17 04:03:30 +0000

    sss

--- pages/3_Download_Data.py
+++ pages/3_Download_Data.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import json
 import base64
+from io import BytesIO
+
+
 session_state = st.session_state
 json_files = session_state.jsonlist
 # Define a list of JSON file paths
